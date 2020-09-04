165147976d4bc805677daba5942fbb8e4f9f1d8b
timmybobguy <41880797+timmybobguy@users.noreply.github.com>
2020-09-04 17:16:34 +1200

    Pushing vue

---
+++ vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+
+      <v-spacer></v-spacer>
+
+
+    </v-app-bar>
+
+    <v-main>
+      <Login/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
